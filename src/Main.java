e6b66d17412f42ec6992737e137c5f6cc47a6c4e
Danila Yarmarkin <danila.iarmarkin@gmail.com>
2020-10-29 15:49:13 +0300

    some changes in dev

--- src/Main.java
+++ src/Main.java
@@ -9,46 +9,10 @@ public class Main {
         mC = new Dot(0,0);
         long end = 1024;
 
-        // Create first line
-        for (long k1 = 1; k1 <= end; k1++){
-            Line line1 = new Line(1/k1);
+        for (int x1 = 2; x1 <= end; x1++){
+            for (int x2 = 1; x2 < end; x2++){
+                for (int y1 = 1; y1 < x1; y1++){
 
-            // Create second line
-            for (float k2 = end; k2 >= 1; k2--){
-                Line line2 = new Line(k2);
-
-                // Travel values on first line
-                for (long x1 = k1; x1 <= end*end*k1; x1+=k1){
-                    long y1 = (long) line1.getY(x1);
-
-                    // Travel values on second line
-                    for (long x2 = 1; x2<=end*k2; x2++){
-                        long y2 = (long) line2.getY(x2);
-
-                        // Init dots
-                        mB.setX(x1);
-                        mB.setY(y1);
-                        mC.setX(x2);
-                        mC.setY(y2);
-
-                        // Create triangle
-                        Triangle t = new Triangle(mA, mB, mC);
-
-                        float ab = t.getAB();
-                        float bc = t.getBC();
-                        float ac = t.getAC();
-
-                        if(ab % 1 == 0 && bc % 1 == 0 && ac % 1 == 0){
-                            System.out.println("A = ("+mA.getX()+";"+mA.getY()+")");
-                            System.out.println("B = ("+mB.getX()+";"+mB.getY()+")");
-                            System.out.println("C = ("+mC.getX()+";"+mC.getY()+")");
-
-                            System.out.println("AB = "+ ab);
-                            System.out.println("BC = "+ bc);
-                            System.out.println("AC = "+ ac);
-                            System.out.println("========================================");
-                        }
-                    }
                 }
             }
         }
